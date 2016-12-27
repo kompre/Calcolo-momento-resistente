29daa8004ae12dda1f2739ae5f96c0c0fc852cd8
kompre <s.follador@gmail.com>
2016-12-27 18:54:00 +0100

    27-12-2016

--- calcoloNM.m
+++ calcoloNM.m
@@ -1,62 +1,82 @@
-function [ N, M ] = calcoloNM( x, sezione, d, A, deformazioni_notevoli, fcd, fyd, tipo)
+function [ N, M ] = calcoloNM( x, sezione, d, A, def_not, fcd, fyd, tipo)
 %CALCOLOFORZE calcola le sollecitazioni interne della sezione M, N
 %   Calcolo del momento resistente M e sforzo normale N per la data sezione
 %   di altezza H e spessore B, nell'ipotesi di deformazione piana della
 %   sezione espressa dalle deformazioni ai lembi della sezione [e1,e2]
 %       x:  posizione asse neutro
-%       sezione:  struttura contenente la coppia di vettori H ed B,
-%       che rappresentano le coordinate rispettivamente lungo l'altezza e
-%       la larghezza della sezione
+%       sezione:  vettore nx2 che contiena la coppia ordinata "h x b" dove
+%       h e b rappresanto rispettivamente l'altezza e la larghezza del
+%       rettangolo i-esimo;
 %       d: vettore che contiene le ordinate le distanze dal bordo
-%       dell'armatura
-%       A: vettore contenente le aree di armatura
-%       deformazioni_notevoli: struttura che contiene tutte le
-%       deformazioni notevoli della sezione in c.a. nel seguente ordine:
+%       dell'armatura;
+%       A: vettore contenente le aree di armatura;
+%       def_not: struttura che contiene tutte le deformazioni notevoli della sezione in c.a. nel seguente ordine:
 %           ecu:    deformazione ultima del cls
 %           ec2:    deformazione al limite elastico per il diagramma parabola rettangolo 
 %           ec3:    deformazione al limite elastico per il diagramma lineare-rettangolo
 %           eyd:    deformazione al limite elastico dell'acciaio
 %           esu:    deformazione al limite ultimo dell'acciaio
-%       fcd: tensione di progetto del cls
-%       fyd: tensione di progetto dell'acciaio
-%       tipo: tipo di analisi svolta ("elastica" o "plastica")
+%       fcd: tensione di progetto del cls;
+%       fyd: tensione di progetto dell'acciaio;
+%       tipo: tipo di analisi svolta ("elastica" o "plastica");
 
 
 %% estrazione dei vettori della struct "sezione"
 
-varnames = fields(sezione);
-h = sezione.(varnames{1});   % il primo campo è riservato all'altezza dei rettangoli
-b = sezione.(varnames{2});   % il secondo campo è riservato alla larghezza dei rettangoli
+h = sezione(:,1);   % il primo campo è riservato all'altezza dei rettangoli
+b = sezione(:,2);   % il secondo campo è riservato alla larghezza dei rettangoli
+
 H = max(h);     % Altezza totale della sezione
 B = max(b);     % Larghezza totale della sezione
-dh = h(2)-h(1); % altezza del rettangolo di base
-db = b(2)-b(1); % larghezza del rettangolo di base
+
+uniqueH = unique(h);    % vettore ordinato dell'altezza della sezione (elimina i valori ripetuti)
+uniqueB = unique(b);    % vettore ordinato della base della sezione (elimina i valori ripetuti
+
+% il vettore delle altezze ha sempre dimesioni 1xN dove N è maggiore di 1,
+% mentre il vettore delle basi può avere dimensione unitaria
+dh = uniqueH(2)-uniqueH(1); % altezza del rettangolo di base
+if length(uniqueB) > 1
+    db = uniqueB(2)-uniqueB(1); % larghezza del rettangolo di base
+else
+    db = uniqueB;
+end
 
 %% estrazione delle deformazioni dalla struct "deformazioni_notevoli"
 % in questo caso le variabili in deformazioni_notevoli devono essere
 % nominare in maniera accurata.
 
-varnames = fields(deformazioni_notevoli);
-for i = 1:length(varnames)
-    assignin('caller',deformazioni_notevoli.(varnames{i}))
+extractField(def_not)
+
+%% estrazione del profilo di deformarzione
+% calcolo delle deformazioni agli estremi della sezione
+switch tipo
+    case 'plastica'
+        [e1, e2] = deformazionePianaSLU(x, H, max(d), ecu, ec2, -esu);
+    case 'elastica'
+        [e1, e2] = deformazionePianaSLE(x, H, max(d), ec3, -eyd);
 end
-
+% creazione del vettore delle deformazioni
+if e1 ~= e2     
+    de = (e1-e2)/(length(uniqueH)-1); % variazione delle deformazioni lungo l'altezza della sezione
+    ev = e2:de:e1;  % vettore delle deformazioni
+else
+    ev = ones(size(uniqueH)) * e1; % se e1 == e2 allora la deformazione è costante lungo tutta l'altezza della sezione
+end
 %%
-[e1, e2] = deformazionePiana(x, H, max(d), ecu, ec2, esu);
-% inizializzo il vettore ev, alla stessa lunghezza del vettore h
-
-
-
-sc = legameCostituivo(ev, 2.0e-3, fcd, 'parabola');   %tensione nel cls
-ss = legameCostituivo(ev, 1.863e-3, fyd, 'bilineare');      %tensione nell'acciaio
+sc = legameCostituivo(ev, ec3, fcd, 'lineare');   %tensione nel cls
+ss = legameCostituivo(ev, eyd, fyd, 'bilineare');      %tensione nell'acciaio
 n = zeros(size(h));
 m = zeros(size(h));
-for i = 1:length(h)
-    n(i) = t * dL * sc(i) + (d==h(i))*A'*ss(i);
-    m(i) = n(i)*(L/2-h(i));
+k = 0;  % indice di ciclo della sezione
+for i = 1:length(uniqueH)
+    for j = 1:length(uniqueB)
+        k = k+1;
+        n(k) = db * dh * sc(i) + sum(and(d==uniqueH(i), j==1) .* A .* ss(i)); % la condizione deve essere verificata per 
+        m(k) = n(k)*(H/2 - h(k));
+    end
 end
-N = sum(n,2)/1e3;
-M = sum(m,2)/1e6;
+N = sum(n);
+M = sum(m);
 
 end
 
--- main.m
+++ main.m
@@ -4,10 +4,11 @@ clc
 % dimensioni sezione in mm
 H = 300;
 B = 1000;
-% altezza dei rettangoli
-dh = 1;
-h = 0:dh:H; % vettore delle coordinate dei rettangoli
-b = ones(size(h))*B;
+rettH = 1001;
+rettB = 10;
+% sollecitazione
+sollecitazioni.Ned = 0;
+sollecitazioni.Med = 100;
 % definizione della posizione delle armature
 d1 = 40;
 d2 = 260;
@@ -19,20 +20,35 @@ def_not.ec2 = 2.0e-3;
 def_not.ec3 = 1.75e-3;
 def_not.esu = 10e-3;
 def_not.eyd = 450/1.15/210e3;
+%% parametri da passare alla funzione calcoloNM
+%sezione = [h;b]';
+d = [d1;d2];
+A = [A1;A2];
+fcd = 0.85*25/1.5;
+fyd = 450/1.15;
+%% Preparazione del vettore sezione
+h = 0;
+pt_nt = [0, d', H]; % punti notevoli in h 
+for i = 2:length(pt_nt)
+    deltaH = pt_nt(i) - pt_nt(i-1);
+    h_ = linspace(pt_nt(i-1),pt_nt(i), deltaH/H * rettH);
+    h = [h h_];
+end
+h = unique(h);
 %%
-[N, M] = calcoloNM(30.28,[h;b]',[d1;d2],[A1;A2],def_not,(0.85*25/1.5),450/1.15,'plastica')
-%%
-x = -1000:2000;
-for i_x = 1:length(x)
-    [e1, e2] = deformazionePiana(x(i_x), L, max(d1,d2), ecu, ec2, esu);
-    de = (e1-e2)/(length(Lv)-1);
-    ev = e2:de:e1;  % vettore delle deformazioni
-    sc = legameCostituivo(ev,2.0e-3,14.17,'parabola');   %tensione nel cls
-    ss = legameCostituivo(ev,1.957e-3,391.3,'bilineare');      %tensione nell'acciaio
-    for i = 1:length(Lv)
-        nsd(i_x,i) = t * dL * sc(i) + (d1==Lv(i))*A1*ss(i) + (d2==Lv(i))*A2*ss(i);
-        msd(i_x,i) = nsd(i_x,i)*(L/2-Lv(i));
+b = linspace(0,B,rettB);
+k = 0;
+sezione = zeros(length(b)*length(h),2);
+for i = 1:length(b)
+    for j = 1:length(h)
+        k = k+1;
+        sezione(k,1) = h(j);
+        sezione(k,2) = b(i);
     end
 end
-Nsd = sum(nsd,2)/1000;
-Msd = sum(msd,2)/1e6;
+
+
+%% metodo dicotomico
+dicotomico('x','[N, M]', 'calcoloNM(x,sezione,d,A,def_not,fcd,fyd,''elastica'');', sollecitazioni.Ned, -1e9, +1e9, 12)
+
+
