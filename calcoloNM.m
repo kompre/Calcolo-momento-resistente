deaff46b6666b9f8f7cd5d80866616fde4c5b368
kompre <s.follador@gmail.com>
2016-12-28 12:04:26 +0100

    Completato la soluzione per il calcolo delle sollecitazioni nel caso della flessione retta

--- calcoloNM.m
+++ calcoloNM.m
@@ -4,9 +4,10 @@ function [ N, M ] = calcoloNM( x, sezione, d, A, def_not, fcd, fyd, tipo)
 %   di altezza H e spessore B, nell'ipotesi di deformazione piana della
 %   sezione espressa dalle deformazioni ai lembi della sezione [e1,e2]
 %       x:  posizione asse neutro
-%       sezione:  vettore nx2 che contiena la coppia ordinata "h x b" dove
-%       h e b rappresanto rispettivamente l'altezza e la larghezza del
-%       rettangolo i-esimo;
+%       sezione:  vettore Nx4 che contiene i vettori ordinati [xm; ym; db;
+%       dh] dove la coppia (xm, ym) sono le coordinate del baricentro del
+%       rettangolo i-esimo, mentre la coppia (db, dh) ne repparesentano le
+%       dimensioni infinitesime.
 %       d: vettore che contiene le ordinate le distanze dal bordo
 %       dell'armatura;
 %       A: vettore contenente le aree di armatura;
@@ -23,22 +24,24 @@ function [ N, M ] = calcoloNM( x, sezione, d, A, def_not, fcd, fyd, tipo)
 
 %% estrazione dei vettori della struct "sezione"
 
-h = sezione(:,1);   % il primo campo è riservato all'altezza dei rettangoli
-b = sezione(:,2);   % il secondo campo è riservato alla larghezza dei rettangoli
+xm = sezione(:,1);  % il primo campo alla coordinata in xm del baricentro dei rettangoli
+ym = sezione(:,2);  % il secondo campo è riservato alla coordinata in ym del baricentro dei rattngoli
+db = sezione(:,3);  % il terzo campo è riservato alla larghezza in x dei rettangoli
+dh = sezione(:,4);  % il quarto campo è riservato alla larghezza in y dei rettangoli
 
-H = max(h);     % Altezza totale della sezione
-B = max(b);     % Larghezza totale della sezione
+H = sum(dh);    % massima altezza della sezione
 
-uniqueH = unique(h);    % vettore ordinato dell'altezza della sezione (elimina i valori ripetuti)
-uniqueB = unique(b);    % vettore ordinato della base della sezione (elimina i valori ripetuti
-
-% il vettore delle altezze ha sempre dimesioni 1xN dove N è maggiore di 1,
-% mentre il vettore delle basi può avere dimensione unitaria
-dh = uniqueH(2)-uniqueH(1); % altezza del rettangolo di base
-if length(uniqueB) > 1
-    db = uniqueB(2)-uniqueB(1); % larghezza del rettangolo di base
-else
-    db = uniqueB;
+%% inserimento delle coordindate dell'aramtura
+for i = 1:length(d)
+    % cerca se d(i) è gia presente nel vettore delle ordinate ym,
+    % altrimenti lo aggiunge in coda, mettendo a 0 db e dh, in modo tale
+    % da contare esclusivamente il contributo dell'armatuta
+    if isempty(find(ym==d(i),1))
+        xm = [xm; 0];
+        ym = [ym; d(i)];
+        db = [db; 0];
+        dh = [dh; 0];
+    end
 end
 
 %% estrazione delle deformazioni dalla struct "deformazioni_notevoli"
@@ -55,25 +58,15 @@ switch tipo
     case 'elastica'
         [e1, e2] = deformazionePianaSLE(x, H, max(d), ec3, -eyd);
 end
-% creazione del vettore delle deformazioni
-if e1 ~= e2     
-    de = (e1-e2)/(length(uniqueH)-1); % variazione delle deformazioni lungo l'altezza della sezione
-    ev = e2:de:e1;  % vettore delle deformazioni
-else
-    ev = ones(size(uniqueH)) * e1; % se e1 == e2 allora la deformazione è costante lungo tutta l'altezza della sezione
-end
 %%
-sc = legameCostituivo(ev, ec3, fcd, 'lineare');   %tensione nel cls
-ss = legameCostituivo(ev, eyd, fyd, 'bilineare');      %tensione nell'acciaio
-n = zeros(size(h));
-m = zeros(size(h));
-k = 0;  % indice di ciclo della sezione
-for i = 1:length(uniqueH)
-    for j = 1:length(uniqueB)
-        k = k+1;
-        n(k) = db * dh * sc(i) + sum(and(d==uniqueH(i), j==1) .* A .* ss(i)); % la condizione deve essere verificata per 
-        m(k) = n(k)*(H/2 - h(k));
-    end
+n = zeros(size(ym));
+m = zeros(size(ym));
+for i = 1:length(ym)
+    ev = e1 - (e1 - e2)*ym(i)/H;    % deformazione nel punto ym(i)
+    sc = legameCostituivo(ev, ec3, fcd, 'lineare');   %tensione nel cls
+    ss = legameCostituivo(ev, eyd, fyd, 'bilineare'); %tensione nell'acciaio
+    n(i) = db(i) * dh(i) * sc + (d==ym(i))' * A * ss; 
+    m(i) = n(i)*(H/2 - ym(i));
 end
 N = sum(n);
 M = sum(m);
--- main.m
+++ main.m
@@ -2,18 +2,19 @@
 clearvars
 clc
 % dimensioni sezione in mm
-H = 300;
-B = 1000;
-rettH = 1001;
-rettB = 10;
+B = [1000];
+H = [300];
+x0 = [0];
+y0 = [0];
+rettH = 1000;
+rettB = 1;
+sezione = rettangolo(B, H, x0, x0, rettB, rettH);
 % sollecitazione
 sollecitazioni.Ned = 0;
 sollecitazioni.Med = 100;
 % definizione della posizione delle armature
-d1 = 40;
-d2 = 260;
-A1 = 565;
-A2 = 565;
+d = [40;260];
+A = [565;565];
 % definizione della curva di deformazione
 def_not.ecu = 3.5e-3;
 def_not.ec2 = 2.0e-3;
@@ -21,33 +22,8 @@ def_not.ec3 = 1.75e-3;
 def_not.esu = 10e-3;
 def_not.eyd = 450/1.15/210e3;
 %% parametri da passare alla funzione calcoloNM
-%sezione = [h;b]';
-d = [d1;d2];
-A = [A1;A2];
 fcd = 0.85*25/1.5;
 fyd = 450/1.15;
-%% Preparazione del vettore sezione
-h = 0;
-pt_nt = [0, d', H]; % punti notevoli in h 
-for i = 2:length(pt_nt)
-    deltaH = pt_nt(i) - pt_nt(i-1);
-    h_ = linspace(pt_nt(i-1),pt_nt(i), deltaH/H * rettH);
-    h = [h h_];
-end
-h = unique(h);
-%%
-b = linspace(0,B,rettB);
-k = 0;
-sezione = zeros(length(b)*length(h),2);
-for i = 1:length(b)
-    for j = 1:length(h)
-        k = k+1;
-        sezione(k,1) = h(j);
-        sezione(k,2) = b(i);
-    end
-end
-
-
 %% metodo dicotomico
 dicotomico('x','[N, M]', 'calcoloNM(x,sezione,d,A,def_not,fcd,fyd,''elastica'');', sollecitazioni.Ned, -1e9, +1e9, 12)
 
